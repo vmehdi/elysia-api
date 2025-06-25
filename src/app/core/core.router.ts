17e2bbb31ddfb9d019637d17557f4839c72a67e8
mehdi vaezi <mh.vaezi@gmail.com>
2025-06-26 01:47:34 +0330

    :lipstick: update /track

--- src/app/core/core.router.ts
+++ src/app/core/core.router.ts
@@ -31,15 +31,21 @@ const coreRouter = new Elysia()
         common: t.Object({
           fingerprint: t.String(),
           tab_id: t.String(),
-          userAgent: t.String(),
-          url: t.String(),
-          referrer: t.String(),
+          is_bot: t.Boolean(),
+          incognito: t.Boolean(),
+          lang: t.String(),
+          screen: t.Number(),
+          cookies: t.Array(t.String()),
+          site_height: t.Number()
         }),
         events: t.Array(
           t.Object({
             type: t.String(),
+            url: t.String(),
+            orientation: t.String(),
             value: t.Unknown(),
             timestamp: t.Number(),
+            scroll: t.Number(),
             sequential_id: t.Number(),
           })
         ),
