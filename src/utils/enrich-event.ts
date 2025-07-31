8c0cee929142637397d8b4eaac80624037e7d944
mehdi vaezi <mh.vaezi@gmail.com>
2025-07-31 17:54:57 +0330

    :pencil: update enriched method

--- src/utils/enrich-event.ts
+++ src/utils/enrich-event.ts
@@ -1,11 +1,14 @@
-
 export function enrichEvent(payload: any, meta: { ip?: string; headers?: any }) {
+  const headers = meta.headers || {};
+  const ua = headers?.get ? headers.get("user-agent") : headers?.["user-agent"] || null;
+  const re = headers?.get ? headers.get("referer") : headers?.["referer"] || null;
+
   return {
     ...payload,
     eid: crypto.randomUUID(),
     ip: meta.ip || null,
-    ua: meta.headers?.get("user-agent") || null,
-    re: meta.headers?.get("referer") || null,
+    ua,
+    re,
     tss: Date.now(),
   };
 }
